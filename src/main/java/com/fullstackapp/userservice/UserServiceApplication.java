4497f6ae5f93c5b6614ef4a868664babd151eb58
Mintbox23 <35546080+Mintbox23@users.noreply.github.com>
2022-03-12 19:56:56 -0500

    test

---
+++ src/main/java/com/fullstackapp/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fullstackapp.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
