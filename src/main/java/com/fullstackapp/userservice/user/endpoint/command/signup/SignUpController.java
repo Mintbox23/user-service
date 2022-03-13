383ab82fcf18fd9a02972bce1839831687f19e46
Mintbox23 <35546080+Mintbox23@users.noreply.github.com>
2022-03-12 21:25:20 -0500

    Added the service skeleton

---
+++ src/main/java/com/fullstackapp/userservice/user/endpoint/command/signup/SignUpController.java
@@ -0,0 +1,10 @@
+package com.fullstackapp.userservice.user.endpoint.command.signup;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SignUpController {
+
+
+
+}
